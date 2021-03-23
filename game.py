3ab140d3ef3ae5c352d4aa7459e343ca752385a9
Kimsh <ksh03117@gmail.com>
2021-03-23 16:20:46 +0900

    깃첫번째

---
+++ game.py
@@ -0,0 +1,14 @@
+print("갤러그 게임 시작")
+print("적 비행기 발생")
+print("1.발사 2.오른쪽 이동 3.왼쪽 이동")
+number = int(input("숫자를 입력하세요: "))
+print("당신의 입력값?",number)
+#만약애 1번을 누르면 총알발사
+if number==1:
+    print("총알발사")
+#만약애 2번을 누르면 오른쪽
+if number==2:
+    print("오른쪽으로이동")
+#만약애 3번을 누르면 왼쪽
+if number==3:
+    print("왼쪽으로이동")
